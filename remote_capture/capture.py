9298d66fab5eefaa95ad6691a5c652516a432b1b
wassfila <wassim.filali@gmail.com>
2018-07-16 17:25:57 +0200

    fix uri and its usage

--- remote_capture/capture.py
+++ remote_capture/capture.py
@@ -8,4 +8,4 @@ os.system(f'iio_attr -q -i -u "{prm["uri"]}" -c ad9361-phy voltage0 rf_bandwidth
 os.system(f'iio_attr -q -i -u "{prm["uri"]}" -c ad9361-phy voltage0 sampling_frequency {prm["sampling_frequency"]}')
 os.system(f'iio_attr -q -i -u "{prm["uri"]}" -c ad9361-phy voltage0 gain_control_mode {prm["gain_control_mode"]}')
 
-os.system(f'iio_readdev -b {prm["buffer_size"]} -s {prm["nb_samples"]} cf-ad9361-lpc > {prm["out_file"]}')
+os.system(f'iio_readdev -u "{prm["uri"]}" -b {prm["buffer_size"]} -s {prm["nb_samples"]} cf-ad9361-lpc > {prm["out_file"]}')
